3fcdbd90d3a31482408afdc5f664333d55ab66ca
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2020-11-02 17:01:57 +0100

    Expose `VirtualUserTarget.webApplication`

--- src/main/kotlin/com/atlassian/performance/tools/virtualusers/api/config/VirtualUserTarget.kt
+++ src/main/kotlin/com/atlassian/performance/tools/virtualusers/api/config/VirtualUserTarget.kt
@@ -3,7 +3,10 @@ package com.atlassian.performance.tools.virtualusers.api.config
 import java.net.URI
 
 class VirtualUserTarget(
-    internal val webApplication: URI,
+    /**
+     * @since 3.12.0
+     */
+    val webApplication: URI,
     internal val userName: String,
     internal val password: String
 )
